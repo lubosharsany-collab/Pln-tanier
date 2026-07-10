45b35b1ba94d50660b179fbf6d3457e508e35cdd
lubosharsany-collab <lubosharsany@gmail.com>
2026-07-10 10:52:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='plny-tanier-v73';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=7.3','./manifest.webmanifest','./icon.svg']))) });
+const CACHE='plny-tanier-v74';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=7.4','./manifest.webmanifest','./icon.svg']))) });
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
